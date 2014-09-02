e4bf246206453d63704bceb8a80c0bd01e12f9da
patrick <patrick@nosfair.com>
2014-09-02 14:24:01 +0200

    merge to origin master

--- lib/Elastica/Query/Filtered.php
+++ lib/Elastica/Query/Filtered.php
@@ -40,46 +40,46 @@ class Filtered extends AbstractQuery {
 	}
 
 	/**
-	 * Sets a query
-	 *
-	 * @param  \Elastica\Query\AbstractQuery $query Query object
-	 * @return \Elastica\Query\Filtered      Current object
-	 */
-	public function setQuery(AbstractQuery $query) {
-		$this->_query = $query;
+     * Sets a query
+     *
+     * @param  \Elastica\Query\AbstractQuery $query Query object
+     * @return \Elastica\Query\Filtered      Current object
+     */
+    public function setQuery(AbstractQuery $query = null)
+    {
+        return $this->setParam('query', $query);
+    }
 
-		return $this;
-	}
+    /**
+     * Sets the filter
+     *
+     * @param  \Elastica\Filter\AbstractFilter $filter Filter object
+     * @return \Elastica\Query\Filtered        Current object
+     */
+    public function setFilter(AbstractFilter $filter = null)
+    {
+        return $this->setParam('filter', $filter);
+    }
 
-	/**
-	 * Sets the filter
-	 *
-	 * @param  \Elastica\Filter\AbstractFilter $filter Filter object
-	 * @return \Elastica\Query\Filtered        Current object
-	 */
-	public function setFilter(AbstractFilter $filter) {
-		$this->_filter = $filter;
+    /**
+     * Gets the filter.
+     *
+     * @return \Elastica\Filter\AbstractFilter
+     */
+    public function getFilter()
+    {
+        return $this->getParam('filter');
+    }
 
-		return $this;
-	}
-
-	/**
-	 * Gets the filter.
-	 *
-	 * @return \Elastica\Filter\AbstractFilter
-	 */
-	public function getFilter() {
-		return $this->_filter;
-	}
-
-	/**
-	 * Gets the query.
-	 *
-	 * @return \Elastica\Query\AbstractQuery
-	 */
-	public function getQuery() {
-		return $this->_query;
-	}
+    /**
+     * Gets the query.
+     *
+     * @return \Elastica\Query\AbstractQuery
+     */
+    public function getQuery()
+    {
+        return $this->getParam('query');
+    }
 
 	/**
 	 * Converts query to array
@@ -95,10 +95,6 @@ class Filtered extends AbstractQuery {
 
 		if ($this->hasParam('filter') && $this->getParam('filter') instanceof AbstractFilter) {
 			$filtered['filter'] = $this->getParam('filter')->toArray();
-		}
-
-		if (empty($filtered)) {
-			throw new InvalidException('A query and/or filter is required');
 		}
 
 		return array('filtered' => $filtered);
